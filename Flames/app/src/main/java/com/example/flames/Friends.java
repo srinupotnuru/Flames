b17fb05c1712cf982c532b97ec7a758dc4d1c8d3
srinupotnuru <47898485+srinupotnuru@users.noreply.github.com>
2019-09-25 15:34:41 +0530

    Flames

---
+++ Flames/app/src/main/java/com/example/flames/Friends.java
@@ -0,0 +1,14 @@
+package com.example.flames;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Friends extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends);
+    }
+}
